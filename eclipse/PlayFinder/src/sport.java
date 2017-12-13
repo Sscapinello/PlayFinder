8391a6713ebc8448be4dccea4143a32c26668168
itsadmin <itsadmin@192.168.0.74>
2017-12-13 17:36:24 +0100

    primo commit

---
+++ eclipse/PlayFinder/src/sport.java
@@ -0,0 +1,18 @@
+
+public class sport {
+
+	private String nSport;
+	private int nPartecipanti;
+	public String getnSport() {
+		return nSport;
+	}
+	public void setnSport(String nSport) {
+		this.nSport = nSport;
+	}
+	public int getnPartecipanti() {
+		return nPartecipanti;
+	}
+	public void setnPartecipanti(int nPartecipanti) {
+		this.nPartecipanti = nPartecipanti;
+	}
+}
---
+++ eclipse/PlayFinder/src/squadra.java
@@ -0,0 +1,21 @@
+
+public class squadra {
+
+	private String nome, nomeSport;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNomeSport() {
+		return nomeSport;
+	}
+
+	public void setNomeSport(String nomeSport) {
+		this.nomeSport = nomeSport;
+	}
+}
