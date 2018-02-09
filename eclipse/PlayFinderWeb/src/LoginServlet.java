b29e8f8b39f4d5db7dfb26d1eba848c0f28775ba
1ITS-master\itsadmin <Sscapinello@github.com>
2018-02-09 17:56:58 +0100

    progetto web

---
+++ eclipse/PlayFinderWeb/src/LoginServlet.java
@@ -0,0 +1,45 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import it.playfinder1.GestioneAccount;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private GestioneAccount ga = new GestioneAccount();
+	private ObjectMapper mapper = new ObjectMapper();
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		EsitoOperazione eo = ga.login(email, password);
+		
+		String json = mapper.writeValueAsString(eo);
+		response.setContentType("application/json");
+		response.getWriter().append(json);
+	}
+
+}
