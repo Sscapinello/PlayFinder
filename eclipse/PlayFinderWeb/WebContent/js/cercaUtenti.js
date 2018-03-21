0bc56b4fbf133540d6947121b2429b47358f7d04
alema <alema@192.168.0.95>
2018-03-21 14:44:46 +0100

    aggiornamento metodi e web

--- eclipse/PlayFinderWeb/WebContent/js/cercaUtenti.js
+++ eclipse/PlayFinderWeb/WebContent/js/cercaUtenti.js
@@ -1,52 +1,50 @@
+var user = localStorage.getItem('utente');
+u = JSON.parse(user);
 	$.ajax({
 		url: 'RicercaUtenti',
 		method: 'get'
 	}).done(function(utenti) {
 		$.each(utenti, function(i, utente) {
+		if(utente.username != u.username){
 		var option = '<li class="form-group">' +
 			            '<label class="col-md-1 control-label" >'+
 			            '</label>'+
 			            '<div class="col-md-10 inputGroupContainer">' +
 		                    '<div class="row input-group margine">'+
-			                       '<span class="input-group-addon">'+
+		                    	   '<span class="input-group-addon">'+
 			                          '<img src="'+utente.profilePicturePath+'" id="imgProfilo" class="img-circle imgProfilo" width="100" height="100">'+
 			                       '</span>'+
-			                       '<a class="col-md-6 evento nEvento linkEvento" href="profilo.html?username='+ utente.username +'">'+
-			                           '<p align = "left" class="col-md-6 evento"style="margin-bottom: 40px;">' + utente.username + 
+			                       '<a class="col-md-12 evento nEvento linkUtente" href="profilo.html?username='+ utente.username +'">'+
+			                           '<p align = "left" class="col-md-6 campo username">' + utente.username + 
 			                           '</p>'+
-			                           '<p>'+utente.nome+' '+ utente.cognome+'</p>'+
-			                           '<p>'+utente.userInEvento.length+'</p>'+
-			                           ''
 			                       '</a>'+
-		                      '</div>'+
+			                       '<div class="col-md-12 campo bottom"><b>Nome e Cognome:</b><p class="nomeCog"> '+utente.nome+' '+ utente.cognome+'</p></div>'+
+			                       '<p class="col-md-12 campo"><b>Et&agrave:</b> '+utente.eta+'</p>'+
+			                       '<p class="col-md-12 campo"><b>Citt&agrave;:</b> '+utente.citta+'</p>'+
+			                       '<p class="col-md-12 campo"><b>Numero Eventi Giocati:</b> '+utente.userInEvento.length+'</p>'+
+			                '</div>'+
 		                 '</div>'+
 		              '</li>';
         $('#listaUtenti').append(option);
+		}
 		});
 		});
 
+	function myFunction() {
+	    var input, filter, ul, li, p, i;
+	    input = document.getElementById("myInput");
+	    filter = input.value.toUpperCase();
+	    ul = document.getElementById("listaUtenti");
+	    li = ul.getElementsByTagName("li");
+	    for (i = 0; i < li.length; i++) {
+	        p = li[i].getElementsByClassName("nomeCog")[0];
+	        t = li[i].getElementsByClassName("username")[0];
+	        if (p.innerHTML.toUpperCase().indexOf(filter) > -1 || t.innerHTML.toUpperCase().indexOf(filter) > -1 ) {
+	            li[i].style.display = "";
+	        } else {
+	            li[i].style.display = "none";
 
-
-
-
-
-
-
-
-
-function myFunction() {
-    var input, filter, ul, li, p, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("listaUtenti");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        p = li[i].getElementsByClassName("utente")[0];
-        if (p.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-
-        }
-    }
-}
+	        }
+	    
+	    }
+	}
